d69fde409ce4a9c891d5aaa4d06d223888fde93d
Lucien Wang <lucienwang@qq.com>
2023-03-15 21:49:36 +0800

    augment components by random flip left and right

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -667,13 +667,16 @@ class LoadImagesAndLabels(Dataset):
         else:
             # Load image
             img, (h0, w0), (h, w) = self.load_image(index)
+            labels = self.labels[index].copy()
+            self.argument_component(img, labels, h, w,
+                                    self.hyp.get('component_id', None),
+                                    self.hyp.get('component_fliplr', None))
 
             # Letterbox
             shape = self.batch_shapes[self.batch[index]] if self.rect else self.img_size  # final letterboxed shape
             img, ratio, pad = letterbox(img, shape, auto=False, scaleup=self.augment)
             shapes = (h0, w0), ((h / h0, w / w0), pad)  # for COCO mAP rescaling
 
-            labels = self.labels[index].copy()
             if labels.size:  # normalized xywh to pixel xyxy format
                 labels[:, 1:] = xywhn2xyxy(labels[:, 1:], ratio[0] * w, ratio[1] * h, padw=pad[0], padh=pad[1])
 
@@ -747,7 +750,8 @@ class LoadImagesAndLabels(Dataset):
         if not f.exists():
             np.save(f.as_posix(), cv2.imread(self.im_files[i]))
 
-    def load_mosaic(self, index):
+    # NOTE: do mosaic for 3 images that are of size 3K x 1K
+    def load_mosaic3(self, index):
         # YOLOv5 4-mosaic loader. Loads 1 image + 3 random images into a 4-image mosaic
         labels4, segments4 = [], []
         s = self.img_size
@@ -779,6 +783,89 @@ class LoadImagesAndLabels(Dataset):
 
             # Labels
             labels, segments = self.labels[index].copy(), self.segments[index].copy()
+            if labels.size:
+                labels[:, 1:] = xywhn2xyxy(labels[:, 1:], w, h, padw, padh)  # normalized xywh to pixel xyxy format
+                segments = [xyn2xy(x, w, h, padw, padh) for x in segments]
+            labels4.append(labels)
+            segments4.extend(segments)
+
+        # Concat/clip labels
+        labels4 = np.concatenate(labels4, 0)
+        for x in (labels4[:, 1:], *segments4):
+            np.clip(x, 0, 2 * s, out=x)  # clip when using random_perspective()
+        # img4, labels4 = replicate(img4, labels4)  # replicate
+
+        # Augment
+        img4, labels4, segments4 = copy_paste(img4, labels4, segments4, p=self.hyp['copy_paste'])
+        img4, labels4 = random_perspective(img4,
+                                           labels4,
+                                           segments4,
+                                           degrees=self.hyp['degrees'],
+                                           translate=self.hyp['translate'],
+                                           scale=self.hyp['scale'],
+                                           shear=self.hyp['shear'],
+                                           perspective=self.hyp['perspective'],
+                                           border=self.mosaic_border)  # border to remove
+
+        return img4, labels4
+
+    # img: hwc, labels: cls, xywh
+    def argument_component(self, img, label, h, w, component_id: int = None, fliplr: float = 0.5):
+        if component_id is None:
+            return
+        unnorm_loc = xywhn2xyxy(label[:, 1:], w, h)
+        for idx, obj in enumerate(label):
+            cls_id = obj[0]
+            if int(cls_id) == component_id:
+                if random.random() < fliplr:
+                    spike_x1, spike_y1, spike_x2, spike_y2 = unnorm_loc[idx, :].astype(np.int)
+                    img[spike_y1:spike_y2, spike_x1:spike_x2] = np.fliplr(
+                        img[spike_y1:spike_y2, spike_x1:spike_x2]
+                    )
+                    for other_idx in range(label.shape[0]):
+                        if idx == other_idx:
+                            continue
+                        x1, y1, x2, y2 = unnorm_loc[other_idx, :].astype(np.int)
+                        # obj is in spike
+                        if x1 >= spike_x1 - 10 and x2 <= spike_x2 + 10 and y1 >= spike_y1 - 10 and y2 <= spike_y2 + 10:
+                            label[other_idx][1] = 2 * label[idx][1] - label[other_idx][1]
+
+
+    def load_mosaic(self, index):
+        # YOLOv5 4-mosaic loader. Loads 1 image + 3 random images into a 4-image mosaic
+        labels4, segments4 = [], []
+        s = self.img_size
+        yc, xc = (int(random.uniform(-x, 2 * s + x)) for x in self.mosaic_border)  # mosaic center x, y
+        indices = [index] + random.choices(self.indices, k=3)  # 3 additional image indices
+        random.shuffle(indices)
+        for i, index in enumerate(indices):
+            # Load image
+            img, _, (h, w) = self.load_image(index)
+            # Labels
+            labels, segments = self.labels[index].copy(), self.segments[index].copy()
+            self.argument_component(img, labels, h, w,
+                                    self.hyp.get('component_id', None),
+                                    self.hyp.get('component_fliplr', None))
+
+            # place img in img4
+            if i == 0:  # top left
+                img4 = np.full((s * 2, s * 2, img.shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
+                x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
+                x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
+            elif i == 1:  # top right
+                x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
+                x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
+            elif i == 2:  # bottom left
+                x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
+                x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, w, min(y2a - y1a, h)
+            elif i == 3:  # bottom right
+                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
+                x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
+
+            img4[y1a:y2a, x1a:x2a] = img[y1b:y2b, x1b:x2b]  # img4[ymin:ymax, xmin:xmax]
+
+            padw = x1a - x1b
+            padh = y1a - y1b
             if labels.size:
                 labels[:, 1:] = xywhn2xyxy(labels[:, 1:], w, h, padw, padh)  # normalized xywh to pixel xyxy format
                 segments = [xyn2xy(x, w, h, padw, padh) for x in segments]
